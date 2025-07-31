b3ae0d7ecd5f1d1704ebf1d8a941c9c4e0bfa2c3
Patrick <patriick.dev@hotmail.com>
2025-07-31 14:50:37 -0300

    feat: Finalização de componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
-  // O JSX deve ser retornado como a expressao
-  // Use parenteses para envolver o JSX de varias linhas
   return (
     <>
-      <Heading>Olá mundo!</Heading>
+      <Heading>
+        Olá mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
         reprehenderit dolores labore? Reprehenderit dolor sit libero,
