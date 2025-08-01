8dcbb822568ed775585022f9ceec280cd9f5eec6
Patrick <patriick.dev@hotmail.com>
2025-08-01 13:11:33 -0300

    feat: Organizando componentes em pastas padronizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-        reprehenderit dolores labore? Reprehenderit dolor sit libero,
-        accusantium in laudantium aut suscipit optio a perferendis deleniti modi
-        eligendi distinctio laboriosam ipsam!
-      </p>
+      <Container>
+        <section>Logo</section>
+      </Container>
+      <Container>
+        <section>Menu</section>
+      </Container>
     </>
   );
 }
