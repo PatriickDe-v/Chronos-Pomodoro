e8b713caa28fbccb6dc4b7ca914c825bde344236
Patrick <patriick.dev@hotmail.com>
2025-08-04 12:56:10 -0300

    feat: Criando o componente de logo com ícone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -7,7 +8,7 @@ export function App() {
   return (
     <>
       <Container>
-        <section>Logo</section>
+        <Logo />
       </Container>
       <Container>
         <section>Menu</section>
