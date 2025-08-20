27f676bdfc5f51814f48bc03dd63f6bbef0f9c4e
Patrick <patriick.dev@hotmail.com>
2025-08-20 10:45:33 -0300

    feat: Criando um componente de botão reutilizável com opções

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { PlayCircleIcon } from 'lucide-react';
 
 export function App() {
   return (
@@ -38,7 +40,7 @@ export function App() {
             <Cycles />
           </div>
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
       </Container>
